d26d5795eb1b810b63616495984e3895f0613abc
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2024-11-11 16:29:08 +0900

    Deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // Use this if you have issues with image optimization in static export
-    },
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  distDir: 'next',
+  images: {
+    unoptimized: true, // Use this if you have issues with image optimization in static export
+  },
+  basePath: '', // No base path for GitHub Pages root deployment
+  assetPrefix: '', // Leave empty for root-level deployment
+};
+
+export default nextConfig;
